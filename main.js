1aaa06d5fa7012e32c953418ee8e9e21af354ee1
leifgerchberg <leifgerchberg@gmail.com>
2019-10-31 09:43:11 -0400

    new information

--- main.js
+++ main.js
@@ -1,35 +1,37 @@
-const createState = (stateObj) => { 
-    return new Proxy(stateObj, { 
-        set(target, property, value) { 
-            target[property] = value; 
-            render();
-            return true; 
-        }
+const createState = (stateObj) => {
+    return new Proxy(stateObj, {
+      set(target, property, value) {
+        target[property] = value;
+        render();
+        return true;
+      }
     });
-}; 
-const state = createState({ 
-    name: ' ', 
-    lastName: ' ', 
+  };
+  
+  const state = createState({
+    name: '',
+    lastName: '',
     age: null
-}) 
-
-const listeners = document.querySelectorAll('[data-model]'); 
-listeners.forEach(element => { 
-    const name = element.dataset.model; 
-    element.addEventListener('keyup', (event) => { 
-        state[name] = element.value; 
-        console.log(state);
+  });
+  
+  const listeners = document.querySelectorAll('[data-model]');
+  
+  listeners.forEach((element) => {
+    const name = element.dataset.model;
+    element.addEventListener('keyup', (event) => {
+      state[name] = element.value;
+      console.log(state);
     });
-}); 
-
-const render = () => { 
+  });
+  
+  const render = () => {
     const bindings = Array.from(document.querySelectorAll('[data-binding]')).map(
-        e => e.dataset.binding
-
-    ); 
-    bindings.forEach((binding) => { 
-        document.querySelector(`[data-binding = ${binding}]`). innerHTML = state[binding];
-        document.querySelector(`[data-binding = ${binding}]`). value  = state[binding];
-
+      e => e.dataset.binding
+    );
+    bindings.forEach((binding) => {
+      document.querySelector(`[data-binding=${binding}]`).innerHTML = state[binding];
+      document.querySelector(`[data-model=${binding}]`).value = state[binding];
     })
-}
+  }
+  
+  render();
